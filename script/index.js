173953cdeb99e794b2143ce26b82c5fbcc4c1f49
BrunoGaxiola <A01253874@tec.mx>
2024-03-08 14:47:59 -0700

    added column images,to show all images is pending

--- script/index.js
+++ script/index.js
@@ -21,106 +21,133 @@ function setActiveItem(strn) {
 }
 
 function getAllProducts(q) {
-    // removeAllChildNodes(tbody);
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-  
-        let { products } = data;
-        let tbody = document.getElementById("tbody");
-        tbody.innerHTML = `<tr>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Discount %</th>
-              <th>Rating</th>
-              <th>Stock</th>
-              <th>Brand</th>
-              <th>Category</th>
-              <th>Thumbnail</th>
-              <th><Modificar></th>
-              <th><Eliminar></th>
-            </tr>`;
-        
-        for (let i = q-10; i < q; i++) { 
-          // New table row
-          let tr = document.createElement("tr");
-  
-          // Id cell
-          let tdId = document.createElement("td");
-          tdId.textContent = products[i].id;
-          tr.appendChild(tdId);
+  // removeAllChildNodes(tbody);
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
 
-          // Title cell
-          let tdTitle = document.createElement("td");
-          tdTitle.textContent = products[i].title;
-          tr.appendChild(tdTitle);
+      let { products } = data;
+      let tbody = document.getElementById("tbody");
+      tbody.innerHTML = `<tr> 
+            <th>Id</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Discount %</th>
+            <th>Rating</th>
+            <th>Stock</th>
+            <th>Brand</th>
+            <th>Category</th>
+            <th>Thumbnail</th>
+            <th>Images</th>
+            <th><Modificar></th>
+            <th><Eliminar></th>
+          </tr>`;
+      let imageIndex = 0;
+      function slideImages([]){
 
-          // Desc cell
-          let tdDesc = document.createElement("td");
-          tdDesc.textContent = products[i].description;
-          tr.appendChild(tdDesc);
+      }
+      
+      for (let i = q-10; i < q; i++) { 
+        // New table row
+        let tr = document.createElement("tr");
 
-          // Price cell
-          let tdPrice = document.createElement("td");
-          tdPrice.textContent = "$" + products[i].price;
-          tr.appendChild(tdPrice);
+        // Id cell
+        let tdId = document.createElement("td");
+        tdId.textContent = products[i].id;
+        tr.appendChild(tdId);
 
-          // Discount cell
-          let tdDisc = document.createElement("td");
-          // Ejemplo de uso de variables de js con texto ``
-          tdDisc.textContent = `${products[i].discountPercentage}%`;
-          tr.appendChild(tdDisc);
+        // Title cell
+        let tdTitle = document.createElement("td");
+        tdTitle.textContent = products[i].title;
+        tr.appendChild(tdTitle);
 
-          // Rating cell
-          let tdRating = document.createElement("td");
-          tdRating.textContent = products[i].rating;
-          tr.appendChild(tdRating);
+        // Desc cell
+        let tdDesc = document.createElement("td");
+        tdDesc.textContent = products[i].description;
+        tr.appendChild(tdDesc);
 
-          // Stock cell
-          let tdStock = document.createElement("td");
-          tdStock.textContent = products[i].stock;
-          tr.appendChild(tdStock);
+        // Price cell
+        let tdPrice = document.createElement("td");
+        tdPrice.textContent = "$" + products[i].price;
+        tr.appendChild(tdPrice);
 
-          // Brand cell
-          let tdBrand = document.createElement("td");
-          tdBrand.textContent = products[i].brand;
-          tr.appendChild(tdBrand);
+        // Discount cell
+        let tdDisc = document.createElement("td");
+        // Ejemplo de uso de variables de js con texto ``
+        tdDisc.textContent = `${products[i].discountPercentage}%`;
+        tr.appendChild(tdDisc);
 
-          // Category cell
-          let tdCategory = document.createElement("td");
-          tdCategory.textContent = products[i].category;
-          tr.appendChild(tdCategory);
-  
-          // Thumbnail cell
-          let tdThumb = document.createElement("td");
-          let img = document.createElement("img");
-          img.src = products[i].thumbnail;
-          img.width = 50;
-          tdThumb.appendChild(img);
-          tr.appendChild(tdThumb);
-  
-          // Modificar cell
-          let tdMod = document.createElement("td");
-          let btnMod = document.createElement("button");
-          btnMod.textContent = "Modificar";
-          tdMod.appendChild(btnMod);
-          tr.appendChild(tdMod);
-  
-          // Eliminar cell
-          let tdEl = document.createElement("td");
-          let btnEl = document.createElement("button");
-          btnEl.textContent = "Eliminar";
-          tdEl.appendChild(btnEl);
-          tr.appendChild(tdEl);
-  
-          tbody.appendChild(tr);
-        }
-      });
-  }
-  
-  getAllProducts(10);
+        // Rating cell
+        let tdRating = document.createElement("td");
+        tdRating.textContent = products[i].rating;
+        tr.appendChild(tdRating);
+
+        // Stock cell
+        let tdStock = document.createElement("td");
+        tdStock.textContent = products[i].stock;
+        tr.appendChild(tdStock);
+
+        // Brand cell
+        let tdBrand = document.createElement("td");
+        tdBrand.textContent = products[i].brand;
+        tr.appendChild(tdBrand);
+
+        // Category cell
+        let tdCategory = document.createElement("td");
+        tdCategory.textContent = products[i].category;
+        tr.appendChild(tdCategory);
+
+        // Thumbnail cell
+        let tdThumb = document.createElement("td");
+        let img = document.createElement("img");
+        img.src = products[i].thumbnail;
+        img.width = 50;
+        tdThumb.appendChild(img);
+        tr.appendChild(tdThumb);
+
+        //Images cell
+
+        //Previous button
+        /*let tdPrev = document.createElement("td");
+        let prev = document.createElement("button");
+        prev.textContent = "<";
+        tdPrev.appendChild(prev);
+        tr.appendChild(tdPrev);*/
+
+        //Image shown
+        let tdImage = document.createElement("td");
+        let image = document.createElement("img");
+        image.src = products[i].images[0]; // Puedes cambiar el index del array images.
+        image.width = 50;
+        tdImage.appendChild(image);
+        tr.appendChild(tdImage);
+
+        //Next button
+        /*let tdNext = document.createElement("td");
+        let next = document.createElement("button");
+        next.textContent = ">";
+        tdNext.appendChild(next);
+        tr.appendChild(tdNext);*/
+
+        // Modificar cell
+        let tdMod = document.createElement("td");
+        let btnMod = document.createElement("button");
+        btnMod.textContent = "Modificar";
+        tdMod.appendChild(btnMod);
+        tr.appendChild(tdMod);
+
+        // Eliminar cell
+        let tdEl = document.createElement("td");
+        let btnEl = document.createElement("button");
+        btnEl.textContent = "Eliminar";
+        tdEl.appendChild(btnEl);
+        tr.appendChild(tdEl);
+
+        tbody.appendChild(tr);
+      }
+    });
+}
 
 getAllProducts(10);
