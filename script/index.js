890499b78e5c43e37879bde5da2b0585b6798124
FernandaGranadosMonge <138026408+FernandaGranadosMonge@users.noreply.github.com>
2024-03-09 02:02:37 +0000

    Fixed Sending of Form Info and Now Shows Success Alert

--- script/index.js
+++ script/index.js
@@ -5,6 +5,8 @@ function getAllProducts() {
       let { products } = data;
       let tbody = document.getElementById("tbody");
       let btnAdd = document.getElementById("AddProductButton");
+
+      /*For add product button*/
       btnAdd.onclick = () => setModalAdd();
       btnAdd.setAttribute("data-bs-toggle", "modal");
       btnAdd.setAttribute("data-bs-target", "#staticBackdrop");
@@ -207,154 +209,195 @@ function setModalAdd() {
   let modalBody = document.getElementById("modal-body");
   modalBody.innerHTML = `
   <div class="modal-body p-5 my-0">
-  <form class="needs-validation" autocomplete="off" name="modalForm" novalidate netlify>
+    <form class="needs-validation" autocomplete="off" name="modalForm" novalidate netlify>
 
-  <div class="mb-2">
-    <label for="nameInput" class="form-label">Name</label>
-    <input type="text" class="form-control" id="nameInput" name="nameInput" placeholder="Enter Name" required>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
-    <div class="invalid-feedback">
-      Please enter the product´s name.
-    </div>
-  </div>
-
-  <div class="mb-2">
-    <label for="validationTextarea" class="form-label">Description</label>
-    <textarea class="form-control" id="descriptionInput" placeholder="Enter description" required></textarea>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
-    <div class="invalid-feedback">
-      Please enter the product´s description.
-    </div>
-  </div>
-
-  <div class="row mb-2 g-2">
-    <div class="col">
-      <label for="categoryInput" class="form-label">Category</label>
-      <select class="form-select" id="categoryInput" required>
-        <option selected disabled value="">Choose a category</option>
-      </select>
-      <div class="invalid-feedback">
-        Please select the product´s category.
-      </div>
-    </div>
-    
-    <div class="col">
-      <label for="brandInput" class="form-label">Brand</label>
-      <select class="form-select" id="brandInput" required>
-        <option selected disabled value="">Choose a brand</option>
-      </select>
-      <div class="invalid-feedback">
-        Please select the product´s brand.
-      </div>
-    </div>
-  </div>
-
-  <div class="row mb-2 g-2">
-    <div class="col">
-      <label for="priceInput">Price</label>
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="inputGroupPrepend">$</span>
+      <div class="mb-2">
+        <label for="nameInput" class="form-label">Name</label>
+        <input type="text" class="form-control" id="nameInput" name="nameInput" placeholder="Enter Name" required>
+        <div class="valid-feedback">
+          Looks good!
         </div>
-        <input type="number" class="form-control" id="priceInput"  aria-describedby="inputGroupPrepend" required>
         <div class="invalid-feedback">
-          Please enter the product´s price
+          Please enter the product´s name.
         </div>
       </div>
-    </div>
 
-    <div class="col">
-      <label for="discountInput">Discount</label>
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="inputGroupPrepend">%</span>
+      <div class="mb-2">
+        <label for="descriptionInput" class="form-label">Description</label>
+        <textarea class="form-control" id="descriptionInput" placeholder="Enter description" required></textarea>
+        <div class="valid-feedback">
+          Looks good!
         </div>
-        <input type="number" class="form-control" id="discountInput"  aria-describedby="inputGroupPrepend" required>
         <div class="invalid-feedback">
-        Please enter the product´s discount
+          Please enter the product´s description.
         </div>
       </div>
-    </div>
 
-    <div class="col">
-      <label for="ratingInput">Rating</label>
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="inputGroupPrepend">★</span>
+      <div class="row mb-2 g-2">
+        <div class="col">
+          <label for="categoryInput" class="form-label">Category</label>
+          <select class="form-select" id="categoryInput" required>
+            <option selected disabled value="">Choose a category</option>
+          </select>
+          <div class="invalid-feedback">
+            Please select the product´s category.
+          </div>
         </div>
-        <input type="number" class="form-control" id="ratingInput"  aria-describedby="inputGroupPrepend" required>
-        <div class="invalid-feedback">
-        Please enter the product´s rating
+        
+        <div class="col">
+          <label for="brandInput" class="form-label">Brand</label>
+          <select class="form-select" id="brandInput" required>
+            <option selected disabled value="">Choose a brand</option>
+          </select>
+          <div class="invalid-feedback">
+            Please select the product´s brand.
+          </div>
         </div>
       </div>
-    </div>
+
+      <div class="row mb-2 g-2">
+        <div class="col">
+          <label for="priceInput">Price</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroupPrepend">$</span>
+            </div>
+            <input type="number" class="form-control" id="priceInput"  aria-describedby="inputGroupPrepend" required>
+            <div class="invalid-feedback">
+              Please enter the product´s price
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <label for="discountInput">Discount</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroupPrepend">%</span>
+            </div>
+            <input type="number" class="form-control" id="discountInput"  aria-describedby="inputGroupPrepend" required>
+            <div class="invalid-feedback">
+            Please enter the product´s discount
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <label for="ratingInput">Rating</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroupPrepend">★</span>
+            </div>
+            <input type="number" class="form-control" id="ratingInput"  aria-describedby="inputGroupPrepend" required>
+            <div class="invalid-feedback">
+            Please enter the product´s rating
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <label for="stockInput" class="form-label">Stock</label>
+        <div class="input-group has-validation">
+          <span class="input-group-text" id="inputGroupPrepend">#</span>
+          <input type="number" class="form-control" id="stockInput" aria-describedby="inputGroupPrepend" required>
+          <div class="invalid-feedback">
+            Please choose a username.
+          </div>
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <label for="thumbnailInput">Thumbnail</label>
+        <input id="thumbnailInput" type="file" class="form-control" aria-label="file example">
+        <div class="invalid-feedback">Example invalid form file feedback</div>
+      </div>
+
+      <div class="mb-3">
+        <label for="imagesInput">Images</label>
+        <input id="imagesInput" type="file" class="form-control" aria-label="file example" multiple>
+        <div class="invalid-feedback">Example invalid form file feedback</div>
+      </div>
+
+      <button id="submitBtn" type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
+  `;
 
-  <div class="col-md-4">
-  <label for="stockInput" class="form-label">Stock</label>
-  <div class="input-group has-validation">
-    <span class="input-group-text" id="inputGroupPrepend">#</span>
-    <input type="number" class="form-control" id="stockInput" aria-describedby="inputGroupPrepend" required>
-    <div class="invalid-feedback">
-      Please choose a username.
-    </div>
-  </div>
-</div>
+  // Populate category select
+  fetch('https://dummyjson.com/products/categories')
+    .then(response => response.json())
+    .then(categories => {
+      const selectElement = document.getElementById('categoryInput');
+      categories.forEach(category => {
+        const option = document.createElement('option');
+        option.text = category;
+        option.value = category;
+        selectElement.add(option);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching categories:', error);
+    });
 
-  <div class="mb-3">
-  <label for="thumbnailInput">Thumbnail</label>
-  <input id="thumbnailInput" type="file" class="form-control" aria-label="file example">
-  <div class="invalid-feedback">Example invalid form file feedback</div>
-</div>
+  fetch('https://dummyjson.com/products')
+  .then(response => response.json())
+  .then(data => {
+    const products = data.products;
+    const brands = [];
 
-<div class="mb-3">
-<label for="imagesInput">Images</label>
-<input id="imagesInput" type="file" class="form-control" aria-label="file example" multiple>
-<div class="invalid-feedback">Example invalid form file feedback</div>
-</div>
+    // Extract all unique brands
+    products.forEach(product => {
+      if (!brands.includes(product.brand)) {
+        brands.push(product.brand);
+      }
+    });
 
+    // Populate the select dropdown with brand options
+    const brandInput = document.getElementById('brandInput');
+    brands.forEach(brand => {
+      const option = document.createElement('option');
+      option.value = brand;
+      option.textContent = brand;
+      brandInput.appendChild(option);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
 
-
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-  </div>
-`;
-
+  //VALIDATE FILLED INPUTS
   const form = document.forms['modalForm'];
   form.addEventListener('submit', function(event) {
     const nameInput = document.getElementById('nameInput');
     const descriptionInput = document.getElementById('descriptionInput');
     const categoryInput = document.getElementById('categoryInput');
-
-    if (!nameInput.checkValidity() || !descriptionInput.checkValidity() || !categoryInput.checkValidity()) {
-      event.preventDefault(); 
-      event.stopPropagation(); 
-    }
+    const brandInput = document.getElementById('brandInput');
+    const priceInput = document.getElementById('priceInput');
+    const discountInput = document.getElementById('discountInput');
+    const ratingInput = document.getElementById('ratingInput');
+    const stockInput = document.getElementById('stockInput');
+    const thumbnailInput = document.getElementById('thumbnailInput');
+    const imagesInput = document.getElementById('imagesInput');
+    
+    event.preventDefault(); // para que no redireccione cuando se manda
     form.classList.add('was-validated');
+
+    if (!nameInput.checkValidity() || !descriptionInput.checkValidity() 
+    || !brandInput.checkValidity() || !priceInput.checkValidity() 
+    || !discountInput.checkValidity() || !ratingInput.checkValidity() 
+    || !stockInput.checkValidity() || !thumbnailInput.checkValidity() 
+    || !categoryInput.checkValidity() || !imagesInput.checkValidity()) {
+      event.preventDefault(); 
+      event.stopPropagation();
+      return;
+    }
+    
+    // Posts answers
+    postData();
+    
   }, false);
-  //POST AFTER CLICKING FINISH BUTTON
-  document
-    .getElementById("finishButton")
-    .addEventListener("click", function () {
-
-      // TODO: Add validation for the inputs
-      postData();
-    });
 }
-
-/*
-function hideModal() {
-  document.getElementById("modal-body").setAttribute("aria-hidden", "true");
-  document.getElementById("exampleModal").classList.remove("show");
-  document.getElementById("exampleModal").style.display = "none";
-}
-
-
-*/
 
 function postData() {
   let title = document.getElementById("nameInput").value;
@@ -366,7 +409,6 @@ function postData() {
   let price = document.getElementById("priceInput").value;
   let rating = document.getElementById("ratingInput").value;
   let images = document.getElementById("imagesInput").value;
-  let martin = document.getElementById("martinInput").value;
 
   let data = {
       title,
@@ -378,10 +420,7 @@ function postData() {
       price,
       rating,
       images,
-      martin
   };
-  
-  
 
   fetch('https://dummyjson.com/products/add', {
       method: 'POST',
@@ -393,11 +432,36 @@ function postData() {
   .then(response => response.json())
   .then(data => {
       console.log('Success:', data);
+      //getAllProducts(); //si se fuera a actualizar de verdad
+      showSuccessAlert();
+      $('#staticBackdrop').modal('hide'); // quita el form
   })
   .catch((error) => {
       console.error('Error:', error);
   });
 }
+
+function showSuccessAlert() {
+  
+  const alertElement = document.createElement('div');
+  alertElement.classList.add('alert', 'alert-success');
+  alertElement.setAttribute('role', 'alert');
+  alertElement.textContent = "Operation was successful!";
+
+  document.body.appendChild(alertElement);
+
+  // Quita la alerta
+  setTimeout(() => {
+    alertElement.remove();
+  }, 5000); 
+}
+
+
+/*function hideModal() {
+  document.getElementById("modal-body").setAttribute("aria-hidden", "true");
+  document.getElementById("exampleModal").classList.remove("show");
+  document.getElementById("exampleModal").style.display = "none";
+}*/
 
 
 getAllProducts();
